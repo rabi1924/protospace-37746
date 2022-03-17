2889e23e3d1fcd28e4da41c6e9b68cba6cf03884
rabi1924 <rabi2024@gmail.com>
2022-03-17 17:20:40 +0900

    「さん付け」と「投稿者以外の編集リダイレクト」の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  #before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.all
@@ -22,6 +22,10 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def destroy
